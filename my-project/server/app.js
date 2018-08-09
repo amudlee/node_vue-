2be8ee53383fdcf6bc9cccc3a259c0f46f771fe5
amud <1179009186@qq.com>
2018-08-09 18:14:13 +0800

    使用express,嵌入前端中

---
+++ my-project/server/app.js
@@ -0,0 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');//
+var cookieParser = require('cookie-parser');//获取cookie信息进行转化
+var logger = require('morgan');//日志输出
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);//仅仅针对访问地址是首页就会加载index模块下的数据
+app.use('/users', usersRouter);//仅仅针对访问地址是用户管理的返回
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
